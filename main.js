3bba5862dd00d3361cd7755f812540a9d35d5856
Aparajita-png <77184488+Aparajita-png@users.noreply.github.com>
2021-06-19 11:06:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function color(){
+    window.location="color.html";
+   }
+   function camera(){
+    window.location="camera.html";
+   }
+   function identify(){
+    window.location="identifier.html";
+   }
+   
+    
